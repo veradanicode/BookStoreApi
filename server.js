ddc48437d4a1a483e0f5e0a6134f298381167b16
Vera D <veradanicode@gmail.com>
2025-08-22 22:41:07 +0100

    update commit from index.js to server.js ,connect database

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config
+const express=require('express')
+const connectToDB =require('./database/db')
+
+const app=express()
+const PORT =process.env.PORT||3000
+
+
+//connect to database
+connectToDB();
+
+//middleware
+app.use(express.json())
+
+app.listen(PORT,()=>{
+    console.log(`Book StoreApi is currently running at port ${PORT}`);
+    
+})
