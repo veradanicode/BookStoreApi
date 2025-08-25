ca162539f0e2d9c12d95f1bde7efea5b741bc331
Vera D <veradanicode@gmail.com>
2025-08-25 17:59:20 +0100

    added controller/function handlers  for CRUD operation in bookstore

---
+++ controllers/book-controller.js
@@ -0,0 +1,73 @@
+const Book = require('../models/book')
+
+const getAllBooks = async(req,res) =>{
+    try {
+       const findAllBooks = await Book.find({})
+       if (findAllBooks) {
+        res.status(200).json({
+            success:true,
+            message:"Book found successfully!",
+            data:findAllBooks
+        })
+       }else{
+        res.status(404).jon({
+            success:false,
+            message:"We didn't find any book here o😪",
+        })
+       }
+    } catch (error) {
+        console.log("Error in fetching books😪",error);
+        res.status(500).json({
+            success:false,
+            message:"Something went wrong!Please try again."
+        })
+    }
+
+}
+
+const getSingleBooksByID = async(req,res) =>{
+    
+}
+
+const addNewBook = async(req,res) =>{
+    try {
+        const newBookFormData =new Book(req.body);
+        const newlyCreatedBook = await newBookFormData.save();
+        if (newlyCreatedBook) {
+            res.status(201).json({
+                success:true,
+                message:"Book created suceesfully!",
+                data:newlyCreatedBook
+            })
+        }else{
+        res.status(404).jon({
+            success:false,
+            message:"Error creating book😪",
+        })
+       }
+    } catch (error) {
+        console.log('Error:',error);
+         res.status(500).json({
+            success:false,
+            message:"Something went wrong!Please try again."
+        })
+        
+    }
+}
+
+const updateBook = async(req,res) =>{
+    
+}
+
+const deleteBook = async(req,res) =>{
+    
+}
+
+
+module.exports ={
+    getAllBooks,
+    getSingleBooksByID,
+    addNewBook,
+    updateBook,
+    deleteBook
+}
