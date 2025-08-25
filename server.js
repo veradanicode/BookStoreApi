92cd5b70290c27c5e76a1651efd3a9d5198716ec
Vera D <veradanicode@gmail.com>
2025-08-25 18:00:58 +0100

    imported book router and used it as a pernt route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express=require('express')
 const connectToDB =require('./database/db')
+const bookRoutes =require('./routes/book-route')
 
 const app=express()
 const PORT =process.env.PORT||3000
@@ -11,6 +12,9 @@ connectToDB();
 
 //middleware
 app.use(express.json())
+
+//routes
+app.use('/api/books',bookRoutes)
 
 app.listen(PORT,()=>{
     console.log(`Book StoreApi is currently running at port ${PORT}`);
